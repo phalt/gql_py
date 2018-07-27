ecb870043c85e46849037175851a6c26b142a2f5
Paul Hallett <paul.hallett@babylonhealth.com>
2018-07-27 15:28:50 +0100

    first working version

--- gql_py/gql.py
+++ gql_py/gql.py
@@ -3,7 +3,10 @@
 """Main module."""
 
 import re
-from typing import Mapping, Any, Optional
+from collections import namedtuple
+from typing import Any, Mapping, Optional
+
+import requests
 
 EXTRA_WHITE_SPACE = re.compile(r'\s{2,}')
 
@@ -15,25 +18,87 @@ def normalise_query(query: str) -> str:
     return EXTRA_WHITE_SPACE.sub(' ', query.strip())
 
 
-def camelcase_variables(variables: Mapping[str, Any]) -> Mapping[str, Any]:
+def to_camelcase(variables: Mapping[str, Any]) -> Mapping[str, Any]:
     """
     Transform typical python underscore-style variables
     into camelCased variables that graphQL expects.
     NOTE: Converts nested dictionaries recursively so all keys are camelCased.
     """
     def convert(string):
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', string)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+        # thanks to https://stackoverflow.com/a/1176023/1327062
+        s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', string)
+        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
-    for key, value in
+    converted_dict = {}
+    for key, value in variables.items():
+        if isinstance(value, dict):
+            value = to_camelcase(value)
+        key = convert(key)
+        converted_dict[key] = value
+    return converted_dict
 
 
-def send(
+def prepared_payload(
     *,
-    query: str,
-    variables: Mapping[str, Any] = None,
-    heades: Mapping[str, Any] = None,
-) -> 'GraphQLResponse':
+    cleaned_query: str,
+    camelcased_variables: Optional[Mapping[str, Any]],
+) -> Mapping[str, Any]:
+    return {
+        'query': cleaned_query,
+        'variables': camelcased_variables,
+    }
 
-    cleaned_query = normalise_query(query)
-    camelcase_variables = camelcase_variables(variables)
+
+GraphQLResponse = namedtuple(
+    'GraphQLResponse', ('errors', 'ok', 'data'),
+)
+
+
+class Gql:
+    """
+    Base class for graphQL HTTP queries
+    """
+    def __init__(
+        self, *,
+        api: str,
+        session: Optional['requests.Session'] = None,
+        default_headers: Optional[Mapping[str, Any]] = None,
+    ) -> None:
+        self.api = api
+        self.session = session or requests.Session()
+        if default_headers:
+            self.session.headers.update(default_headers)
+
+    def generate_result(
+        self, result: 'requests.Response'
+
+    ) -> 'GraphQLResponse':
+        json_data = result.json()
+        return GraphQLResponse(
+            errors=json_data.get('errors') or None,
+            ok=result.status_code == 200 and not json_data.get('errors', []),
+            data=json_data.get('data'),
+        )
+
+    def send(
+        self, *,
+        query: str,
+        variables: Optional[Mapping[str, Any]] = None,
+        headers: Mapping[str, Any] = None,
+    ) -> 'GraphQLResponse':
+
+        cleaned_query = normalise_query(query)
+        camelcased_variables = to_camelcase(variables) if variables else None
+        payload = prepared_payload(
+            cleaned_query=cleaned_query,
+            camelcased_variables=camelcased_variables,
+        )
+        all_headers = self.session.headers
+        if headers:
+            all_headers.update(**headers)
+        req = requests.Request(
+            url=self.api, method='POST', json=payload, headers=headers
+        )
+        prepared_request = self.session.prepare_request(req)
+        result = self.session.send(prepared_request)
+        return self.generate_result(result)
