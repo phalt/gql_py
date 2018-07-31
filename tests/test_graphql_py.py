c5bc8ca74f4c040c9dfda5f7a6adb69f3cc53189
Paul Hallett <paul.hallett@babylonhealth.com>
2018-07-31 15:22:01 +0100

    Improve tests

--- tests/test_graphql_py.py
+++ tests/test_graphql_py.py
@@ -4,20 +4,80 @@
 """Tests for `gql_py` package."""
 
 import pytest
+import responses
 
-from gql_py import Gql
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+from gql_py import Gql, to_camelcase
 
 
-def test_object(response):
+def test_basic_object():
     gql = Gql(api='http://test.com')
     assert gql.api == 'http://test.com'
+
+
+@responses.activate
+def test_headers_are_set():
+    responses.add(responses.POST, 'http://test.com/graphql',
+                  body='''
+                    {"errors": [], "data": {"title": "blog title"}}''',
+                  status=200,
+                  content_type='application/json')
+    gql = Gql(api='http://test.com/graphql')
+    gql.send(query='query \{\}', variables={}, headers={'foo': 'bar'})
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == 'http://test.com/graphql'
+    assert 'foo' in responses.calls[0].request.headers.keys()
+
+
+@responses.activate
+def test_headers_are_set_during_object_creation():
+    responses.add(responses.POST, 'http://test.com/graphql',
+                  body='''
+                    {"errors": [], "data": {"title": "blog title"}}''',
+                  status=200,
+                  content_type='application/json')
+    gql = Gql(api='http://test.com/graphql', default_headers={'foo': 'bar'})
+    gql.send(query='query {}', variables={})
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == 'http://test.com/graphql'
+    assert 'foo' in responses.calls[0].request.headers.keys()
+
+
+@responses.activate
+def test_good_response():
+    responses.add(responses.POST, 'http://test.com/graphql',
+                  body='''
+                    {"errors": [], "data": {"title": "blog title"}}''',
+                  status=200,
+                  content_type='application/json')
+    gql = Gql(api='http://test.com/graphql')
+    result = gql.send(query='query \{\}', variables={}, headers={'foo': 'bar'})
+    assert result.errors is None
+    assert result.ok
+    assert result.data == {'title': 'blog title'}
+
+
+@responses.activate
+def test_bad_response():
+    responses.add(responses.POST, 'http://test.com/graphql',
+                  body='''
+                    {"errors": [{"message": "some error"}], "data": {}}''',
+                  status=200,
+                  content_type='application/json')
+    gql = Gql(api='http://test.com/graphql')
+    result = gql.send(query='query \{\}', variables={}, headers={'foo': 'bar'})
+    assert result.errors == [{'message': 'some error'}]
+    assert not result.ok
+    assert result.data is None
+
+
+
+@pytest.mark.parametrize('input, output', [
+    (
+        {'some_variable': 'value'}, {'someVariable': 'value'}
+    ),
+    (
+        {'nested': {'change_me': 'value'}}, {'nested': {'changeMe': 'value'}}
+    )
+])
+def test_to_camel_case(input, output):
+    assert to_camelcase(input) == output
